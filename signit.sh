b9bb93b0d9aa9d7d2308f5d5dd9f3638e490534a
Jon Pry <jonpry@gmail.com>
2018-05-04 14:10:00 -0400

    fix sigs

--- signit.sh
+++ signit.sh
@@ -3,13 +3,14 @@ mkdir -p work/contents
 rm work/* -f -R
 mkdir -p work/contents/control
 mkdir -p work/contents/data
+rm signed.ipk
 
 filename=$(basename -- "$1")
 cp $1 ./work/$filename.gz
 gzip -d ./work/$filename.gz
 tar xf ./work/$filename -C ./work/contents
 tar zxf ./work/contents/control.tar.gz -C ./work/contents/control
-tar zxf ./work/contents/data.tar.gz -C ./work/contents/data
+tar zxfp ./work/contents/data.tar.gz -C ./work/contents/data
 mkdir -p work/contents/data/etc
 cd work/contents/data
 hash=$(find -type f | xargs sha256sum)
@@ -17,7 +18,9 @@ cd ../../..
 echo "$hash" > foo.sums
 sha256sum ./foo.sums | cut -d' ' -f1 >> foo.sums 
 cp foo.sums ./work/contents/data/etc/helloworld.sig
-tar zcf ./work/contents/data.tar.gz -C ./work/contents/data .
+tar zcfv ./work/contents/data.tar.gz -C ./work/contents/data .
+#tar zcfv --atime-preserve ./work/contents/data.tar.gz -C ./work/contents/data .
+
 rm ./work/contents/data -R -f
 rm ./work/contents/control -R -f
 tar zcf ./signed.ipk -C ./work/contents/ .
